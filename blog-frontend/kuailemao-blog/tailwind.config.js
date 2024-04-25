2a273b511dca32d819acb86a2b3e9a84f2a4087e
kuailemao <3490223402@qq.com>
2024-04-25 18:15:24 +0800

    tailwindcss重构文章列表，新增响应式移动端文章列表，取消加载字体文件

--- blog-frontend/kuailemao-blog/tailwind.config.js
+++ blog-frontend/kuailemao-blog/tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'selector', // or 'media' or 'class'
   theme: {
     extend: {},
   },
