1be5b2ae9627bbe350d0648d360dbad472a6c4de
kuailemao <3490223402@qq.com>
2024-05-09 18:25:06 +0800

    编写前端用户设置页面，修复tailwind与element-plus的样式冲突问题

--- blog-frontend/kuailemao-blog/src/main.ts
+++ blog-frontend/kuailemao-blog/src/main.ts
@@ -4,12 +4,16 @@ import App from '@/App.vue'
 import 'virtual:svg-icons-register'
 // 引入自定义插件对象：注册整个项目全局组件
 import gloablComponent from '@/components'
-import 'element-plus/dist/index.css'
 import router from '@/router'
 // 引入模板的全局的样式
 import '@/styles/index.scss'
 // tailwind.css  https://www.tailwindcss.cn/docs
 import "@/styles/tailwind.css";
+/**
+ *  element-plus 必须要在 tailwind 后导入，不然会出现样式冲突导致按钮背景色透明等问题
+ *  具体请访问问题：https://github.com/tailwindlabs/tailwindcss/discussions/5969
+ */
+import 'element-plus/dist/index.css'
 // 黑暗模式
 import 'element-plus/theme-chalk/dark/css-vars.css'
 // pinia
--- blog-frontend/kuailemao-blog/src/views/Setting/index.vue
+++ blog-frontend/kuailemao-blog/src/views/Setting/index.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import {ref} from 'vue'
 import {ElMessage} from 'element-plus'
-import {Plus, User} from '@element-plus/icons-vue'
+import {Plus, User, Select, Message, Refresh} from '@element-plus/icons-vue'
 
 import type {UploadProps} from 'element-plus'
 
-const radio = ref(6)
+const radio = ref('保密')
 
 const imageUrl = ref('')
 
@@ -31,83 +30,111 @@ const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
 <template>
   <Header/>
   <div class="flex justify-center items-center">
-    <div class="mt-16 w-[80rem]">
-      <Main is-side-bar>
-        <template #content>
+    <div class="md:mt-16 mt-10 2xl:w-[100rem] w-full flex md:flex-row flex-col justify-center">
+      <div class="md:w-1/2 w-full">
+        <div class="bg-white w-full p-5 rounded shadow shadow-slate-300">
           <div>
-            <div>
-              <el-icon>
-                <User/>
-              </el-icon>
-              <span class="font-bold text-xl ml-2">账号信息设置</span>
-            </div>
-            <span style="color: gray;font-size: 0.8rem">在这里可以编辑你的个人信息，个人简介等等……</span>
-            <el-divider style="margin-top: 0.5rem"/>
-            <div class="flex justify-center">
-              <div class="w-1/2 mb-5">
-                <div class="flex justify-center">
-                  <el-upload
-                      class="avatar-uploader"
-                      action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-                      :show-file-list="false"
-                      :on-success="handleAvatarSuccess"
-                      :before-upload="beforeAvatarUpload"
-                  >
-                    <img v-if="imageUrl" :src="imageUrl" class="avatar"/>
-                    <el-icon v-else class="avatar-uploader-icon">
-                      <Plus/>
-                    </el-icon>
-                  </el-upload>
-                </div>
-                <div class="flex justify-center">
-                  <el-form
-                      label-position="top"
-                      label-width="auto"
-                      class="w-full mt-5"
-                  >
-                    <el-form-item label="用户昵称">
-                      <el-input  />
-                    </el-form-item>
-                    <el-form-item label="性别">
-                      <el-radio-group v-model="radio">
-                        <el-radio :value="3" label="男"/>
-                        <el-radio :value="6" label="女"/>
-                        <el-radio :value="9" label="未知"/>
-                      </el-radio-group>
-                    </el-form-item>
-                    <el-form-item label="个人简介">
-                      <el-input type="textarea"/>
-                    </el-form-item>
-                  </el-form>
-                </div>
-                <el-button type="danger" >Success</el-button>
+            <el-icon>
+              <User/>
+            </el-icon>
+            <span class="font-bold text-xl ml-2">账号信息设置</span>
+          </div>
+          <span style="color: gray;font-size: 0.8rem">在这里可以编辑你的个人信息，个人简介等等……</span>
+          <el-divider style="margin-top: 0.5rem"/>
+          <div class="flex justify-center mx-6">
+            <div class="w-full mb-5">
+              <div class="flex justify-center">
+                <el-upload
+                    class="avatar-uploader"
+                    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess"
+                    :before-upload="beforeAvatarUpload"
+                >
+                  <img v-if="imageUrl" :src="imageUrl" class="avatar" alt="头像"/>
+                  <el-icon v-else class="avatar-uploader-icon">
+                    <Plus/>
+                  </el-icon>
+                </el-upload>
               </div>
+              <div class="flex justify-center">
+                <el-form
+                    label-position="top"
+                    label-width="auto"
+                    class="w-full mt-5"
+                >
+                  <el-form-item label="用户昵称">
+                    <el-input placeholder="请输入用户昵称"/>
+                  </el-form-item>
+                  <el-form-item label="性别">
+                    <el-radio-group v-model="radio">
+                      <el-radio label="男">男</el-radio>
+                      <el-radio label="女">女</el-radio>
+                      <el-radio label="保密">保密</el-radio>
+                    </el-radio-group>
+                  </el-form-item>
+                  <el-form-item label="个人简介">
+                    <el-input type="textarea" placeholder="请输入个人简介"/>
+                  </el-form-item>
+                </el-form>
+              </div>
+              <el-button type="success" :icon="Select">更新信息</el-button>
             </div>
           </div>
-        </template>
-        <template #information>
-          <div class="ml-10 w-[20rem]" style="min-height: 20px;z-index: 1000;">
-            <div class="bg-white rounded" style="border: 1px solid #dcdfe6">
-              <div style="text-align: center;padding: 15px 15px 10px 15px">
-                <el-avatar :size="70" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"/>
-                <div style="font-weight: bold">
-                  你好，mao
-                </div>
-                <el-divider style="margin: 10px 0"/>
-                <div style="font-size: 14px;color: grey;padding: 10px">
-                  {{ '这个用户很懒，没有填写个人简介~' }}
-                </div>
+        </div>
+        <div class="bg-white w-full p-5 mt-5 rounded shadow shadow-slate-300">
+          <div>
+            <el-icon>
+              <Message/>
+            </el-icon>
+            <span class="font-bold text-xl ml-2">电子邮件设置</span>
+          </div>
+          <span style="color: gray;font-size: 0.8rem">在这里可以编辑你的个人信息，个人简介等等……</span>
+          <el-divider style="margin-top: 0.5rem"/>
+          <div class="flex justify-center">
+            <div class="w-full mb-5">
+              <div class="flex justify-center mx-6">
+                <el-form
+                    label-position="top"
+                    label-width="auto"
+                    class="w-full mt-5"
+                >
+                  <el-form-item label="电子邮件">
+                    <el-input placeholder="请输入电子邮件"/>
+                  </el-form-item>
+                  <el-form-item>
+                    <div class="flex w-full">
+                      <el-input placeholder="请获取验证码"/>
+                      <el-button type="success" plain class="ml-2">获取验证码</el-button>
+                    </div>
+                  </el-form-item>
+                </el-form>
               </div>
-            </div>
-            <div class="mt-5 p-3 bg-white rounded" style="border: 1px solid #dcdfe6">
-              <div>账号注册时间：1970/1/1 08:00:00</div>
-              <div style="color: grey">
-                欢迎加入个人博客！
-              </div>
+              <el-button class="ml-6" type="success" :icon="Refresh">更新信息</el-button>
             </div>
           </div>
-        </template>
-      </Main>
+        </div>
+      </div>
+      <div class="md:ml-10 md:w-[20rem] w-full p-5 " style="min-height: 20px;position: sticky;top: 20px">
+        <div class="bg-white rounded" style="border: 1px solid #dcdfe6">
+          <div style="text-align: center;padding: 15px 15px 10px 15px">
+            <el-avatar :size="70" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"/>
+            <div style="font-weight: bold">
+              你好，mao
+            </div>
+            <el-divider style="margin: 10px 0"/>
+            <div style="font-size: 14px;color: grey;padding: 10px">
+              {{ '这个用户很懒，没有填写个人简介~' }}
+            </div>
+          </div>
+        </div>
+        <div class="mt-5 p-3 bg-white rounded" style="border: 1px solid #dcdfe6">
+          <div>账号注册时间：1970/1/1 08:00:00</div>
+          <div style="color: grey">
+            欢迎加入个人博客！
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
