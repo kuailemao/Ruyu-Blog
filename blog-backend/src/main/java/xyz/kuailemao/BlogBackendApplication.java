4265076579bac4561ff1c9fe6aceea7d8e2c36eb
kuailemao <3490223402@qq.com>
2024-05-20 15:33:24 +0800

    新增后端成功启动输出

--- blog-backend/src/main/java/xyz/kuailemao/BlogBackendApplication.java
+++ blog-backend/src/main/java/xyz/kuailemao/BlogBackendApplication.java
@@ -1,5 +1,6 @@
 package xyz.kuailemao;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +9,27 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @MapperScan("xyz.kuailemao.mapper")
 @SpringBootApplication
 @EnableMethodSecurity
+@Slf4j
 public class BlogBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogBackendApplication.class, args);
-    }
 
+        log.info(
+                """
+                \n
+                ---------------------------------------------------------恭喜你成功启动后端---------------------------------------------------------
+                  _____                          _     _
+                 |  __ \\                        | |   | |           \s
+                 | |__) |   _ _   _ _   _ ______| |__ | | ___   __ _\s
+                 |  _  / | | | | | | | | |______| '_ \\| |/ _ \\ / _` |
+                 | | \\ \\ |_| | |_| | |_| |      | |_) | | (_) | (_| |
+                 |_|  \\_\\__,_|\\__, |\\__,_|      |_.__/|_|\\___/ \\__, |
+                               __/ |                            __/ |
+                              |___/                            |___/
+
+
+                """
+        );
+    }
 }
