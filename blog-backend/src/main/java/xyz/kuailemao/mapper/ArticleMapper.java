f9ed88b7c716c694063a65fa6a4a3cb827096a51
kuailemao <3490223402@qq.com>
2024-08-01 20:27:48 +0800

    修复sql查询已删除文章错误

--- blog-backend/src/main/java/xyz/kuailemao/mapper/ArticleMapper.java
+++ blog-backend/src/main/java/xyz/kuailemao/mapper/ArticleMapper.java
@@ -17,6 +17,6 @@ import java.util.List;
  */
 public interface ArticleMapper extends BaseMapper<Article> {
 
-    @Select("SELECT * FROM t_article WHERE status = #{status} ORDER BY RAND() LIMIT #{limit}")
+    @Select("SELECT * FROM t_article WHERE status = #{status} and is_deleted = 0 ORDER BY RAND() LIMIT #{limit}")
     List<Article> selectRandomArticles(@Param("status") Integer status, @Param("limit") Integer limit);
 }
