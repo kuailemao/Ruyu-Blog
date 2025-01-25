6ce2ce61ea17925a8c68cb60076c1c43b58a6ed6
kuailemao <3490223402@qq.com>
2025-01-25 14:23:57 +0800

    fix: 添加参数校验

--- blog-backend/src/main/java/xyz/kuailemao/domain/dto/PhotoAlbumDTO.java
+++ blog-backend/src/main/java/xyz/kuailemao/domain/dto/PhotoAlbumDTO.java
@@ -1,7 +1,12 @@
 package xyz.kuailemao.domain.dto;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
-import lombok.experimental.Accessors;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * @author kuailemao
@@ -10,7 +15,12 @@ import lombok.experimental.Accessors;
  */
 @Data
 public class PhotoAlbumDTO {
+
     private Long parentId;
+
+    @NotEmpty(message = "相册名称不能为空")
+    @Size(max = 10, message = "相册名称不能超过10个字符")
     private String name;
+
     private String description;
 }
